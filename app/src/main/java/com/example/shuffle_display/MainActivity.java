cd626ffb754ca179b17565a96a3a7217d806486e
Manjari thapa <thapamanjari40@gmail.com>
2019-04-04 11:35:10 +0545

    displays output

--- app/src/main/java/com/example/shuffle_display/MainActivity.java
+++ app/src/main/java/com/example/shuffle_display/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
+tvOutput.setText(shuffel(etWord.getText().toString()));
     }
 
 }
